4c2ab7f2c9de8fb1c591e6bd654951596d709eba
Tiago Dantas <tiagodantas@outlook.com>
2014-11-01 13:05:04 -0300

    fix IndentationError 2

--- HowYouGit/core/views.py
+++ HowYouGit/core/views.py
@@ -19,17 +19,17 @@ def index(request):
         return render(request, 'index.html')
 
 def location(request):
-	if request.method == 'POST':
-		location = request.POST['location']
-		location = location.replace(" ", "+")
+    if request.method == 'POST':
+        location = request.POST['location']
+        location = location.replace(" ", "+")
 
-		github_service = GitHubService()
+        github_service = GitHubService()
 
-		users = github_service.get_location_users(location)
+        users = github_service.get_location_users(location)
         languages = github_service.get_location_language_statistics(location)
 
         location = location.replace("+", " ")
 
-		return render(request, 'location_stats.html', { 'location' : location, 'users' : users, 'languages' : languages })
-	else:
-		return render(request, 'location.html')
+        return render(request, 'location_stats.html', { 'location' : location, 'users' : users, 'languages' : languages })
+    else:
+        return render(request, 'location.html')
